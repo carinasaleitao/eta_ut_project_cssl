df7bbefa068d3cd0b0987b0ce329143d9a29a9f0
carina <carinasaleitao@gmail.com>
2023-10-01 10:01:49 -0300

    add unit tests for restaurant and ice_cream_stand

--- test/models/test_ice_cream_stand.py
+++ test/models/test_ice_cream_stand.py
@@ -1,10 +1,68 @@
+from src.models.ice_cream_stand import IceCreamStand
+
+
 class TestIceCreamStand:
 
-    def test_flavors_available(self):
-        assert False
+    def test_flavors_available_existent_stock(self):
+        icecreamstand = IceCreamStand('Mamma Mia', 'comida italiana', ['limao', 'pistache', 'tiramisu'])
+        response_assert = 'No momento temos os seguintes sabores de sorvete disponíveis:\t-limao,\tpistache,\t-tiramisu'
 
-    def test_find_flavor(self):
-        assert False
+        response = icecreamstand.flavors_available()
 
-    def test_add_flavor(self):
-        assert False
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_flavors_available_out_of_stock(self):
+        icecreamstand = IceCreamStand('Mamma Mia', 'comida italiana', [])
+        response_assert = 'Estamos sem estoque atualmente!'
+
+        response = icecreamstand.flavors_available()
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_find_flavor_existent_flavor(self):
+        icecreamstand = IceCreamStand('Mamma Mia', 'comida italiana', ['limao', 'pistache', 'tiramisu'])
+        response_assert = 'Temos no momento limao!'
+
+        response = icecreamstand.find_flavor('limao')
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_find_flavor_nonexistent_flavor(self):
+        icecreamstand = IceCreamStand('Mamma Mia', 'comida italiana', ['limao', 'pistache', 'tiramisu'])
+        response_assert = 'Não temos no momento baunilha!'
+
+        response = icecreamstand.find_flavor('baunilha')
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_find_flavor_nonexistent_stock(self):
+        icecreamstand = IceCreamStand('Mamma Mia', 'comida italiana', [])
+        response_assert = 'Estamos sem estoque atualmente!'
+
+        response = icecreamstand.find_flavor('limao')
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_add_flavor_existent_flavor(self):
+        icecreamstand = IceCreamStand('Mamma Mia', 'comida italiana', ['limao', 'pistache', 'tiramisu'])
+        response_assert = 'Sabor já disponivel!'
+
+        response = icecreamstand.find_flavor('limao')
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_add_flavor_nonexistent_flavor(self):
+        icecreamstand = IceCreamStand('Mamma Mia', 'comida italiana', ['limao', 'pistache', 'tiramisu'])
+        response_assert = 'Baunilha adicionado ao estoque!'
+
+        response = icecreamstand.find_flavor('baunilha')
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_add_flavor_nonexistent_stock(self):
+        icecreamstand = IceCreamStand('Mamma Mia', 'comida italiana', [])
+        response_assert = 'Estamos sem estoque atualmente!'
+
+        response = icecreamstand.find_flavor('limao')
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
--- test/models/test_restaurant.py
+++ test/models/test_restaurant.py
@@ -1,16 +1,69 @@
+from src.models.restaurant import Restaurant
+
+
 class TestRestaurant:
 
     def test_describe_restaurant(self):
-        assert False
+        restaurant = Restaurant('mamma mia', 'comida italiana')
+        self.number_served = 10
+        self.open = True
 
-    def test_open_restaurant(self):
-        assert False
+        response_assert = "Esse restaurante se chama Mamma Mia and serve comida italiana.\nEsse restaturante está servindo 10 consumidores desde que está aberto."
 
-    def test_close_restaurant(self):
-        assert False
+        response = restaurant.describe_restaurant()
 
-    def test_set_number_served(self):
-        assert False
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
 
-    def test_increment_number_served(self):
-        assert False
+    def test_open_restaurant_closed(self):
+        restaurant = Restaurant('mamma mia', 'comida italiana')
+        self.open = False
+        response_assert = 'Mamma Mia agora está aberto!'
+
+        response = restaurant.open_restaurant()
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_close_restaurant_open(self):
+        restaurant = Restaurant('mamma mia', 'comida italiana')
+        self.open = True
+        response_assert = 'Mamma Mia já está aberto!'
+
+        response = restaurant.open_restaurant()
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_set_number_served_open(self):
+        restaurant = Restaurant('mamma mia', 'comida italiana')
+        self.open = True
+        response_assert = 10
+
+        response = restaurant.set_number_served(10)
+
+        assert response_assert == response, 'Retorno do código difere do número de clientes atendidos esperado.'
+
+    def test_set_number_served_closed(self):
+        restaurant = Restaurant('mamma mia', 'comida italiana')
+        self.open = False
+        response_assert = 'Mamma Mia está fechado!'
+
+        response = restaurant.set_number_served(10)
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
+
+    def test_increment_number_served_positive(self):
+        restaurant = Restaurant('mamma mia', 'comida italiana')
+        self.number_served = 0
+        response_assert = 1
+
+        response = restaurant.increment_number_served(1)
+
+        assert response_assert == response, 'Retorno do código difere do número de clientes atendidos esperado.'
+
+    def test_increment_number_served_negative(self):
+        restaurant = Restaurant('mamma mia', 'comida italiana')
+        self.number_served = 0
+        response_assert = 'Numero invalido'
+
+        response = restaurant.increment_number_served(-1),
+
+        assert response_assert == response, 'Retorno do código difere da mensagem esperada.'
